c4c81265f87ec7e6d78ecdd444dc15001f31b6ba
Leo Zenon <leozenon@ufrn.edu.br>
2020-04-03 00:00:29 -0300

    Updates

---
+++ precache-manifest.ac5ad7649a2eb193b636f405c1899e86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8884df9ad5dbb6b575a42d90c79ae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8118f52db35a5cd310f5",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e2af6198df151de5edcd",
+    "url": "/static/css/main.766362d6.chunk.css"
+  },
+  {
+    "revision": "8118f52db35a5cd310f5",
+    "url": "/static/js/2.1e52676b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1e52676b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2af6198df151de5edcd",
+    "url": "/static/js/main.a5436e90.chunk.js"
+  },
+  {
+    "revision": "a61f284bc19a650dbade",
+    "url": "/static/js/runtime-main.3c96f85d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
